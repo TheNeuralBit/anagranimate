54ea84c9e5f0830d6c985bd4971d6e47afdea945
Brian Hulette <hulettbh@vt.edu>
2015-11-15 19:51:35 -0500

    now swaps centroids properly when mouse enters

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ var svg = panel.append('svg:svg').attr('width', w).attr('height', h)
 var str1 = strParse('the neural bit');
 var str2 = strParse('brian hulette');
 
+
 function strParse(str) {
+  var x = d3.scale.ordinal()
+        .domain(d3.range(str.length))
+        .rangePoints([0, w], 1);
   var rtrn = []
   var char_count = {};
   for (var i = 0; i < str.length; i++) {
@@ -16,7 +20,9 @@ function strParse(str) {
     }
     rtrn.push({
       c: str[i],
-      count: char_count[str[i]]
+      count: char_count[str[i]],
+      cx: x(i),
+      cy: h/2
     });
   }
   return rtrn;
@@ -39,26 +45,60 @@ svg.selectAll('text')
       return d.c;
     });
 
+var force = d3.layout.force()
+  .nodes(str1)
+  .links([])
+  .gravity(0)
+  .size([w, h])
 
-function toggle(str) {
-  return svg.selectAll('text')
-    .data(str, key)
-    .transition()
-    .duration(1000)
-    .attr('x', function (d, i) {
-      return i * 10;
-    })
-    .attrTween('y', function (d, i) {
-      var peak = Math.random() * h - h/2;
-      return function (t) {
-        return h/2 - (-4 * peak * Math.pow(t, 2) + 4 * peak * t);
-      }
-    });
+// Move nodes toward cluster focus.
+function gravity(alpha) {
+  return function(d) {
+    d.y += (d.cy - d.y) * alpha;
+    d.x += (d.cx - d.x) * alpha;
+  };
 }
 
-var active = false;
+force.on("tick", function(e) {
+  svg.selectAll("text")
+    .each(gravity(.2*e.alpha))
+    .attr("x", function(d) { 
+      return d.x;
+    })
+    .attr("y", function(d) { 
+      return d.y;
+    });
+});
 
+force.start();
+
+function toggle(str) {
+  force.nodes(str);
+
+  text = svg.selectAll('text');
+  old_str = text.data();
+  old_str.forEach(function(item) {
+    str.forEach(function(other) {
+      if (key(item) == key(other)) {
+        other.x = item.x;
+        other.y = item.y
+        other.px = item.px;
+        other.py = item.py;
+      }
+    });
+  });
+  text.data(str, key);
+  force.start();
+}
+
+
+
+var active = true;
 svg.on('mouseover', function() { 
-  toggle(str2); 
-  toggle(str1).delay(2000);
+  if (active) {
+    toggle(str2); 
+  } else {
+    toggle(str1);
+  }
+  active = !active;
 })
