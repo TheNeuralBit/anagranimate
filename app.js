7f58f0e5c0a9c207ec0b6a06d5c47f309146f52d
Brian Hulette <hulettbh@vt.edu>
2015-11-16 20:07:14 -0500

    letters now avoid the mouse

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var str2 = strParse('brian hulette');
 function strParse(str) {
   var x = d3.scale.ordinal()
         .domain(d3.range(str.length))
-        .rangePoints([0, w], 1);
+        .rangePoints([20, w-20], 1);
   var rtrn = []
   var char_count = {};
   for (var i = 0; i < str.length; i++) {
@@ -45,11 +45,14 @@ svg.selectAll('text')
       return d.c;
     });
 
+mouse_pos = {}
+
 var force = d3.layout.force()
-  .nodes(str1)
+  .nodes([mouse_pos].concat(str1))
   .links([])
   .gravity(0)
   .size([w, h])
+  .charge(0);
 
 // Move nodes toward cluster focus.
 function gravity(alpha) {
@@ -61,7 +64,7 @@ function gravity(alpha) {
 
 force.on("tick", function(e) {
   svg.selectAll("text")
-    .each(gravity(.2*e.alpha))
+    .each(gravity(.1*e.alpha))
     .attr("x", function(d) { 
       return d.x;
     })
@@ -73,7 +76,7 @@ force.on("tick", function(e) {
 force.start();
 
 function toggle(str) {
-  force.nodes(str);
+  force.nodes([mouse_pos].concat(str));
 
   text = svg.selectAll('text');
   old_str = text.data();
@@ -94,11 +97,30 @@ function toggle(str) {
 
 
 var active = true;
-svg.on('mouseover', function() { 
+svg.on('mouseenter', function() { 
+  console.log(this)
+  console.log('mouseenter');
   if (active) {
+    console.log('brian hulette')
     toggle(str2); 
   } else {
+    console.log('the neural bit')
     toggle(str1);
   }
+  force.charge(function(d, i) { return i == 0 ? -1000 : 1; });
   active = !active;
-})
+  force.start();
+});
+
+svg.on('mouseleave', function() { 
+  console.log('mouseleave');
+  force.charge(0);
+  force.start();
+});
+
+svg.on('mousemove', function() { 
+  var p1 = d3.mouse(this);
+  mouse_pos.x = p1[0];
+  mouse_pos.y = p1[1];
+  force.resume();
+});
